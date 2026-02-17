9ca8610f91ce38b8e0b9be7ccc680249de85fb87
Dominik Jelínek <djelinek@redhat.com>
2026-02-17 13:02:33 +0100

    chore: use Mocha json reporter (#319)

--- .mocharc.js
+++ .mocharc.js
@@ -1,4 +1,8 @@
 // increase default test case timeout to 5 seconds
 module.exports = {
 	timeout: 5000,
+	reporter: 'json',
+    'reporter-option': {
+        output: './reports/ui-test-results.json'
+    }
 };
